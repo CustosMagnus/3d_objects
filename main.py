a9372a634693f188a2bbca3eb5e41f9ed53a6543
CustosMagnus <mcarle@rsnweb.ch>
2022-02-18 10:46:55 +0100

    lichtbrechungswinkel berechnet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import pandas as pd
 import math
 
 class main():
@@ -11,6 +10,7 @@ class main():
         # Circle -> (x-M_x)**2 + (y-M_y)**2 + (z)**2 = radius**2 -> M_z == 0
         self.M_x = 250
         self.M_y = 250
+        self.M_z = 0
         self.radius = 100
         # light source
         self.ls_M_x = 400
@@ -23,10 +23,10 @@ class main():
                 if math.sqrt((abs(x - self.M_x))**2+(abs(y - self.M_y))**2) < self.radius:
                     img[x][y] = [255, 255, 255]
                     z = math.sqrt(self.radius ** 2 - (y - self.M_y) ** 2 - (x - self.M_x) ** 2)
-                    # perpendicular line = [x-self.M_x, y-self.M_y, z]*t+[x, y, z]
+                    # perpendicular line = [x-self.M_x, y-self.M_y, z-self.M_z]*t+[x, y, z]
                     # light ray line = [x-self.ls_M_x, y-self.ls_M_y, z-self.ls_M_z]*t+[self.ls_M_x, self.ls_M_y, self.ls_M_z]
-                    alpha = math.acos(((x - self.M_x) * (x - self.ls_M_x) + (y - self.M_y) * (y - self.ls_M_y) + (z) * (z - self.ls_M_z)) /)
-
+                    # folgende formel rechnet den einfallswinkel bzw ausfallswinkel des lichtstrahls auf die Kugeloberfläche aus
+                    alpha = math.acos(((x - self.M_x) * (x - self.ls_M_x) + (y - self.M_y) * (y - self.ls_M_y) + (z-self.M_z) * (z - self.ls_M_z)) / (math.sqrt((x-self.M_x)**2+(y-self.M_y)**2+(z-self.M_z)**2)*math.sqrt((x-self.ls_M_x)**2+(y-self.ls_M_y)**2+(z-self.ls_M_z)**2)))
 
         return img
 
